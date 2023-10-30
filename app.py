2de68b1e34ab27d279dbd82e6cb0cb54236dcac2
Kiana Salter <kiana3177@gmail.com>
2023-10-30 18:44:31 +0000

    help[

--- app.py
+++ app.py
@@ -10,16 +10,36 @@ def hello():                      # method called hello
 
     url = "https://best-booking-com-hotel.p.rapidapi.com/booking/best-accommodation"
 
-    querystring = {"cityName":"Berlin","countryName":"Germany"}
+    querystrings = [
+    {"cityName": "Berlin", "countryName": "Germany"},
+    {"cityName": "Atlanta", "countryName": "Georgia"}
+    ]
+   #querystring = {"cityName":"Berlin","countryName":"Germany"}
+    #querystring2 = {"cityName":"Atlanta","countryName":"Georgia"}
 
     headers = {
-	"X-RapidAPI-Key": "38aba8fdd7msh17e0b0cef497d80p12ff81jsnae1951e99dcd",
+	"X-RapidAPI-Key": "4894c3a1bcmshd133c8566ef57e7p1c5692jsn39cf5eb18188",
 	"X-RapidAPI-Host": "best-booking-com-hotel.p.rapidapi.com"
     }
+    all_data = []
 
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
-    return render_template("index.html", datum=data)
+    for querystring in querystrings:
+        response = requests.get(url, headers=headers, params=querystring)
+
+    #data = response.json()
+        #if response.status_code != 200:
+            #return f"Error: Received status code {response.status_code} from API."
+        data = response.json()
+        all_data.append(data)
+    return all_data
+
+# Display the data
+    for item in all_data:
+        print(item)
+
+    #response = requests.get(url, headers=headers, params=querystring)
+    #data = response.json()
+    #return render_template("index.html", datum=data)
 
 if __name__ == '__main__':
     app.run(debug=True) 
